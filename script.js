9cac8255e817b17145015056ee70018372075727
ilikeronnnnii <megatzafran@gmail.com>
2024-06-10 12:45:04 +0800

    habis login & signup

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
 
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("signInButton")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the form from submitting
+      window.location.href = "applications.html"; // Redirect to the new HTML page
+    });
+});
